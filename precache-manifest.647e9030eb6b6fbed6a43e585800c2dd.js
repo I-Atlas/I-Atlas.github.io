ac4dead16581dcfb2a7ca7e4c2be44dd51ca37fc
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2020-11-05 19:09:11 +0300

    Updates

---
+++ precache-manifest.647e9030eb6b6fbed6a43e585800c2dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a0bc55ffa7593d8296f403c47155da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d7dae634017c009ae1",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "add5bbc87d0205dfb1ee",
+    "url": "/static/css/main.8394113e.chunk.css"
+  },
+  {
+    "revision": "b0d7dae634017c009ae1",
+    "url": "/static/js/2.09c662cc.chunk.js"
+  },
+  {
+    "revision": "36f1cdd90bea500ed9ac90f4ecb51123",
+    "url": "/static/js/2.09c662cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add5bbc87d0205dfb1ee",
+    "url": "/static/js/main.adaad3ac.chunk.js"
+  },
+  {
+    "revision": "4e298d7ae7623fec8b7e",
+    "url": "/static/js/runtime-main.b85d870a.js"
+  },
+  {
+    "revision": "39590cb7cc559384c238bc843c200eb2",
+    "url": "/static/media/github_white.39590cb7.svg"
+  },
+  {
+    "revision": "53d6616c613b48d5154ece51a3694731",
+    "url": "/static/media/iliya.53d6616c.jpg"
+  },
+  {
+    "revision": "9dc06eb7bea90b016409abc5b017eb1b",
+    "url": "/static/media/instagram_white.9dc06eb7.svg"
+  },
+  {
+    "revision": "bf2d8f3b5da688e85c47d6a39f2e31b7",
+    "url": "/static/media/linkedin_white.bf2d8f3b.svg"
+  },
+  {
+    "revision": "87d5b1a5e8dc8438e14633abf7139ca4",
+    "url": "/static/media/telegram_white.87d5b1a5.svg"
+  }
+]);
